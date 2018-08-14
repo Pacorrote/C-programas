c156a02d2dd6f9a5e0604bf429c3afbdea1b4310
Pacorrote <42156807+Pacorrote@users.noreply.github.com>
2018-08-13 20:58:38 -0500

    Add files via upload

---
+++ buscarelemento.c
@@ -0,0 +1,12 @@
+int buscarElemento (int elemento){
+	nodo * aux = inicio;
+	while ((aux!=NULL)&&(aux.dato!=elemento)){
+		aux=aux.siguiente;
+	}
+	if (aux==NULL){
+		return false;
+	}
+	else{
+		return true;
+	}
+}
---
+++ estaordenada.c
@@ -0,0 +1,19 @@
+int estaOrdenada(){
+	nodo * aux = inicio;
+	nodo * ant = NULL;
+	if(inicio==NULL){
+		return true;
+	}
+	else if (inicio.siguiente == NULL){
+		return true;
+	}
+	else{
+		nodo * ant = inicio;
+		nodo * aux = inicio.siguiente;
+		while ((aux!=NULL) && (ant.dato<=aux.dato)){
+			ant = aux;
+			aux = aux.siguiente;
+		}
+		return aux==NULL? true: false;
+	}
+}
